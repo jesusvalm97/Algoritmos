3a23ba288f5dc819a9849fe24e6dd73dee72103e
jesusvalm97 <52518344+jesusvalm97@users.noreply.github.com>
2020-08-26 13:22:12 -0500

    Creacion de un pdf con header y sub header

--- ConsoleAppPDF/ConsoleAppPDF/Program.cs
+++ ConsoleAppPDF/ConsoleAppPDF/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using iText.Layout.Properties;
 
 namespace ConsoleAppPDF
 {
@@ -10,6 +14,21 @@ namespace ConsoleAppPDF
     {
         static void Main(string[] args)
         {
+            PdfWriter writer = new PdfWriter("C:\\Users\\Developer\\Desktop\\demo.pdf");
+            PdfDocument pdf = new PdfDocument(writer);
+            Document document = new Document(pdf);
+
+            Paragraph header = new Paragraph("HEADER")
+                .SetTextAlignment(TextAlignment.CENTER)
+                .SetFontSize(20);
+            document.Add(header);
+
+            Paragraph subHeader = new Paragraph("SUB HEADER")
+                .SetTextAlignment(TextAlignment.CENTER)
+                .SetFontSize(15);
+            document.Add(subHeader);
+
+            document.Close();
         }
     }
 }
